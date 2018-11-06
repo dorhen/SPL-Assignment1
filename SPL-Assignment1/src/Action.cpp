68053e1c1f1b2142d53b0970c8f0a5b1edaef730
dorhen <44751515+dorhen@users.noreply.github.com>
2018-11-06 20:28:57 +0200

    Update Action.cpp

--- SPL-Assignment1/src/Action.cpp
+++ SPL-Assignment1/src/Action.cpp
@@ -5,10 +5,10 @@
 //BaseAction
 
 //Constructor
-BaseAction::BaseAction() : errorMsg(""), status(PENDING){}
+BaseAction::BaseAction() : errorMsg(""), status(PENDING), argsDelivered(""){}
 //Methods
 ActionStatus BaseAction::getStatus() const{
-    return this.status;
+    return this->status;
 }
 void BaseAction::updateArgs(std::string args) {
     this->argsDelivered = args;
@@ -41,13 +41,14 @@ void OpenTable::act(Restaurant &restaurant) {
         t->openTable();
         for(size_t i = 0; i < t->getCustomers().size() ; i++)
             t->addCustomer(t->getCustomers()[i]);
+        complete();
     }
 }
 std::string OpenTable::toString() const {
     if(this->getStatus() == 2)
-        std::cout << this->getArgs() << " Error: " << this->getErrorMsg() << std::endl;
+        std::cout << "open " <<  this->getArgs() << " Error: " << this->getErrorMsg() << std::endl;
     else
-        std::cout << this->getArgs() << "Completed" << std::endl;
+        std::cout << "open " << this->getArgs() << "Completed" << std::endl;
 }
 
 
@@ -60,38 +61,149 @@ void Order::act(Restaurant &restaurant) {
     Table *t = restaurant.getTable(this->tableId);
         if (!t || !(t->isOpen()))
             this->error("Table does not exist or is already open");
-        else
+        else {
             t->order(restaurant.getMenu());
+            complete();
+        }
+
+}
+std::string Order::toString() const {
+    if(this->getStatus() == 2)
+        std::cout << "order " <<  this->getArgs() << " Error: " << this->getErrorMsg() << std::endl;
+    else
+        std::cout << "order " << this->getArgs() << "Completed" << std::endl;
 }
 
 
 //MoveCustomer
 
 //Constructort 
-MoveCustomer::MoveCustomer(int src, int dst, int customerId) : srcTable(src), dstTable(dst), id(customerId){}
+MoveCustomer::MoveCustomer(int src, int dst, int customerId) :BaseAction(), srcTable(src), dstTable(dst), id(customerId){}
 //Methods
 void MoveCustomer::act(Restaurant &restaurant) {
     Table *t1 = restaurant.getTable(this->srcTable);
     Table *t2 = restaurant.getTable(this->dstTable);
     if(!t1 || !(t1->isOpen()) || !t2 || !(t2->isOpen()) || t2->getCapacity() == t2->getCurrentSize() || !t1->getCustomer(this->id))
         this->error("Cannot move customer");
-    else{
+    else {
         t2.addCustomer(t1.getCustomer(this->id));
         std::vector<OrderPair> toMove = t1->removeOrders(this->id);
-        for(size_t i=0; i < toMove.size(); i++)
-            t2->addOrder(toMove[i]);
+        for (const auto &i : toMove)
+            t2->addOrder(i);
         t1->removeCustomer(this->id);
+        complete();
     }
-})
+}
+std::string MoveCustomer::toString() const {
+    if(this->getStatus() == 2)
+        std::cout << "move " <<  this->getArgs() << " Error: " << this->getErrorMsg() << std::endl;
+    else
+        std::cout << "move " << this->getArgs() << "Completed" << std::endl;
+}
+
+
+//Close
 
 //Constructor
-Close::Close (int id): tableId(id) {}
+Close::Close (int id): BaseAction(), tableId(id) {}
+//Methods
 void Close::act(Restaurant &restaurant){
     Table *t = restaurant.getTable(this->tableId);
     if (!t || !(t->isOpen()))
-        this->error("Table does not exist or is already open");
+        this->error("Table does not exist or is not open");
     else{
+
         std::cout << "Table " << this->tableId << "was closed. " << "Bill " << t->getBill() << "NIS" << std::endl;
         t->closeTable();
+        complete();
     }
+}
+std::string Close::toString() const {
+    if(this->getStatus() == 2)
+        std::cout << "close " <<  this->getArgs() << " Error: " << this->getErrorMsg() << std::endl;
+    else
+        std::cout << "close " << this->getArgs() << "Completed" << std::endl;
+}
+
+
+//CloseAll
+
+//Constructor
+CloseAll::CloseAll(): BaseAction() {}
+//Methods
+void CloseAll::act(Restaurant &restaurant) {
+    Table *t = restaurant.getTable(0);
+    int size = restaurant.getNumOfTables();
+    Close *c;
+    for (size_t i = 0; i < size ; i++){
+        if(!(t->isOpen()))continue;
+        else{
+            c = new Close((int)i);
+            c->act(restaurant);
+            delete c;
+        }
+    }
+    complete();
+}
+std::string CloseAll::toString() const {
+    std::cout << "closeall" << " Completed" << std::endl;
+}
+
+
+//PrintMenu
+
+//Constructor
+PrintMenu::PrintMenu(): BaseAction() {}
+//Methods
+void PrintMenu::act(Restaurant &restaurant) {
+    std::vector<Dish> menu = restaurant.getMenu();
+    for (auto &i : menu)
+        i.toString();
+    complete();
+}
+std::string PrintMenu::toString() const {
+    std::cout << "menu" << " Completed" <<  std::endl;
+}
+
+
+//PrintTableStatus
+
+//Constructor
+PrintTableStatus::PrintTableStatus(int id): BaseAction(), tableId(id) {}
+//Methods
+void PrintTableStatus::act(Restaurant &restaurant) {
+    Table *t = restaurant.getTable(this->tableId);
+    if((!t->isOpen())){
+        std::cout << "Table " << tableId << " status: close" << std::endl;
+        complete();
+    }
+    else {
+        std::cout << "Table " << tableId << " status: open" << std::endl;
+        for (size_t i = 0; i < t->getCurrentSize(); i++)
+            (t->getCustomer((int) i))->toString();
+        std::vector<OrderPair> orders = t->getOrders();
+        for (auto &order : orders)
+            std::cout << order.second.getName() << " " << order.second.getPrice() << "NIS " << order.first << std::endl;
+        complete();
+    }
+}
+std::string PrintTableStatus::toString() const {
+    std::cout << "menu" << this->getArgs() << " Completed" <<  std::endl;
+}
+
+
+//PrintActionsLog
+
+//Constructor
+PrintActionsLog::PrintActionsLog(): BaseAction(){}
+//Methods
+void PrintActionsLog::act(Restaurant &restaurant) {
+    std::vector<BaseAction*> acts = restaurant.getActionsLog();
+    for (auto &act : acts) {
+        act->toString();
+    }
+    complete();
+}
+std::string PrintActionsLog::toString() const {
+    std::cout << "log" << " Completed" <<  std::endl;
 }
