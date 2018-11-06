e32f93bc7b14e240c1099cba7aef5a987588b525
dorhen <44751515+dorhen@users.noreply.github.com>
2018-11-06 20:28:22 +0200

    Update Dish.cpp

--- SPL-Assignment1/src/Dish.cpp
+++ SPL-Assignment1/src/Dish.cpp
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include "../include/Dish.h"
 
 Dish::Dish(int d_id, std::string d_name, int d_price, DishType d_type) : id(d_id), price(d_price), type(d_type), name(d_name) {}
@@ -14,4 +15,7 @@ int Dish::getPrice() const{
 }
 DishType Dish::getType() const{
     return type;
+}
+std::string Dish::toString() const {
+    std::cout<< name << " " << type << " " << price << "NIS" << std::endl;
 }
